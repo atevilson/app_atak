2b72bd26fe0c70a2f212121f71d20e6595b1b0ac
atevilson <freitas.atevilson@gmail.com>
2024-08-15 16:51:43 -0300

    add cores na appbar, button e isolamento de strings

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,15 +4,22 @@ import 'package:app_atak/widgets/result_container.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 class HomeScreen extends StatelessWidget {
   final GlobalKey<ResultContainerState> _resultContainerKey = GlobalKey<ResultContainerState>();
+  HomeScreen({super.key});
+
+  // títulos de widgets
+  final appDePesquisaAtak = "App de pesquisa Atak";
+  final pesquisaGoogle = "Pesquisa google";
+  final pesquisar = "Pesquisar";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('App de pesquisa Atak'),
+        title: Text(appDePesquisaAtak),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -23,7 +30,17 @@ class HomeScreen extends StatelessWidget {
               onChanged: (value) {
                 Provider.of<SearchProvider>(context, listen: false).updateSearchText(value);
               },
-              decoration: const InputDecoration(labelText: 'Pesquisa google'),
+              decoration: InputDecoration(
+                labelText: pesquisaGoogle,
+                iconColor: Colors.black,
+                focusedBorder: const OutlineInputBorder(
+                  gapPadding: 1.0,
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                  )
+                ),
+                floatingLabelStyle: const TextStyle(color: Colors.black),
+              ),
             ),
             const SizedBox(height: 16.0),
             ElevatedButton(
@@ -31,7 +48,13 @@ class HomeScreen extends StatelessWidget {
                 await Provider.of<SearchProvider>(context, listen: false).search();
                 _resultContainerKey.currentState?.refresh();
               },
-              child: const Text('Pesquisar'),
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.black),
+                foregroundColor: MaterialStatePropertyAll(Colors.white),
+              ),
+              child: Text(
+                pesquisar,
+              ),
             ),
             const SizedBox(height: 16.0),
             ResultContainer(key: _resultContainerKey),
